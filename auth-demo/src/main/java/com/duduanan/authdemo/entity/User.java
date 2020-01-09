1ee93d13ab84d8ccc16031731895672dbaf5be50
Jerry Guo <jerry.guo@internal.finpacific.com>
2020-01-09 16:33:03 +1100

    config

---
+++ auth-demo/src/main/java/com/duduanan/authdemo/entity/User.java
@@ -0,0 +1,64 @@
+package com.duduanan.authdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "user_info")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name="lastName")
+    private String lastName;
+    
+    private String email;
+    
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
