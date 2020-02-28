4d4c4c563d6b311ffad717f6fc7cb7a2f110e2cc
Jerry Guo <jerry.guo@internal.finpacific.com>
2020-02-28 16:31:27 +1100

    adding token_key

--- auth-demo/src/main/java/com/duduanan/authdemo/config/WebSecurityConfig.java
+++ auth-demo/src/main/java/com/duduanan/authdemo/config/WebSecurityConfig.java
@@ -1,8 +1,23 @@
 package com.duduanan.authdemo.config;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +26,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import com.duduanan.authdemo.config.entrypoints.CustomEntryPoint;
 
@@ -19,6 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
 
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -53,4 +72,59 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+	
+	@Bean
+	public JwtAccessTokenConverter tokenEnhancer() throws IOException{
+		Resource resource = resourceLoader.getResource("classpath:privateKey");
+		
+		PrivateKey privateKey = loadPrivateKey(resource.getFile());
+		Resource resource1 = resourceLoader.getResource("classpath:certificate.pem");
+		PublicKey publicKey = loadCertificate(resource1.getFile()).getPublicKey();
+		
+		KeyPair keyPair = new KeyPair(publicKey, privateKey);
+		
+	    final JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+	    
+	    jwtAccessTokenConverter.setKeyPair(keyPair);
+	    return jwtAccessTokenConverter;
+	}
+	
+	 /**
+     * Load a PKCS8 key
+     * @param filename The path of the key
+     */
+    private PrivateKey loadPrivateKey(File file) {
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] keyArray = new byte[fis.available()];
+            fis.read(keyArray);
+            keyArray = java.util.Base64.getDecoder().decode(keyArray);
+            PKCS8EncodedKeySpec kspec = new PKCS8EncodedKeySpec(keyArray);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePrivate(kspec);
+        } catch (Exception e) {
+            // throw new SamlException("Couldn't load private key", e);
+        }
+        return null;
+    }
+
+    /**
+     * Load an X.509 certificate
+     * @param filename The path of the certificate
+     */
+    private X509Certificate loadCertificate(File file) {
+        try (FileInputStream fis = new FileInputStream(file);
+                BufferedInputStream bis = new BufferedInputStream(fis)) {
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            return (X509Certificate) cf.generateCertificate(bis);
+
+        } catch (FileNotFoundException e) {
+            // throw new SamlException("Public key file doesn't exist", e);
+        } catch (Exception e) {
+            // throw new SamlException("Couldn't load public key", e);
+        }
+        return null;
+    }
 }
